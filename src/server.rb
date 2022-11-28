6fa9d57acc154da9d4ed20966d52732d40e8a578
Ubuntu <ubuntu@ip-172-31-7-90.ec2.internal>
2022-11-28 03:52:55 +0000

    se trato de hacer scores

--- src/server.rb
+++ src/server.rb
@@ -1,6 +1,9 @@
+require 'json'
+require 'faraday'
 require 'sinatra'
+require 'aws-sdk-dynamodb'
 
-#funct llamar lambda que se conecta a DYNAMO + PROCESOS get/post
+lambdascore = "https://ec26j6i46tsug6hze2eb5jyqui0uhcbe.lambda-url.us-east-1.on.aws/src/models"
 
 get '/' do
     erb :main
@@ -13,5 +16,25 @@ get '/quiz/:n' do
 end
 
 get '/scores' do
+    connection = Faraday.new(url:lambdascore)
+    response = connection.get
+    @scores = []
+
+    if response.success?
+      @scores = JSON.parse(response.body)
+    end
+    puts @scores
     erb :scores
+end
+
+post '/scores' do
+      dynamodb = Aws::DynamoDB::Client.new
+
+      new_item = {
+        nombre: session[:nombre],
+        score: session[:score].to_i
+      }
+
+    dynamodb.put_item(table_name: 'scores', item: new_item)
+    redirect '/scores'
 end
